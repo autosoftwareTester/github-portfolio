a687996c281bb0215bf496e858a84ee594f300f9
igwe <autotestsoftware@gmail.com>
2026-01-29 13:29:31 +0200

    add logs

--- Test/API-Testing/UnitTest1.cs
+++ Test/API-Testing/UnitTest1.cs
@@ -1,15 +1,24 @@
 ﻿namespace API_Testing;
 
+using Microsoft.Extensions.Logging;
+
 public class Tests
 {
+    private ILogger<Tests> _logger;
+
     [SetUp]
     public void Setup()
     {
+        var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+        _logger = loggerFactory.CreateLogger<Tests>();
+        _logger.LogInformation("Test setup started");
     }
 
     [Test]
     public void Test1()
     {
+        _logger.LogInformation("Test1 execution started");
         Assert.Pass();
+        _logger.LogInformation("Test1 passed");
     }
 }
